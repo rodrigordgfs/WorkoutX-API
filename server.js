1281a96b7d98051030d191244cd899859b1435f7
rodrigordgfs <rodrigordgfs@gmail.com>
2025-03-01 04:14:44 -0300

    feature: Validate Clerk Token

--- server.js
+++ server.js
@@ -3,20 +3,30 @@ import cors from "@fastify/cors";
 import routes from "./src/routes/index.js";
 import environment from "./src/config/envs.js";
 import { startJobs } from "./src/jobs/index.js";
+import { clerkPlugin } from "@clerk/fastify";
 
 startJobs();
 
 const app = fastify({
-  pluginTimeout: 60000
+  pluginTimeout: 60000,
 });
 
 app.register(cors, {
   origin: "*",
 });
+
+app.register(clerkPlugin, {
+  secretKey: process.env.CLERK_SECRET_KEY,
+  publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+});
+
 app.register(routes);
 
 app
-  .listen({ port: environment.port, host: "192.168.8.4" })
+  .listen({
+    port: environment.port,
+    host: process.env.host,
+  })
   .then(() => {
     console.log(`Server is running on port ${environment.port}`);
   })
